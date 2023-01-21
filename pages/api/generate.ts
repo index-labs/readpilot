1edba350bae417330649cd3b2757eeb68b5c34de
Jiayuan <forrestchang7@gmail.com>
2023-01-21 14:11:36 +0800

    feat: add openai api

---
+++ pages/api/generate.ts
@@ -0,0 +1,50 @@
+import type { NextRequest } from "next/server";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: NextRequest): Promise<Response> => {
+  if (req.method !== "POST") {
+    return new Response("Method not allowed", { status: 405 });
+  }
+
+  const { url } = (await req.json()) as {
+    url?: string;
+  };
+
+  if (!url) {
+    return new Response("Invalid request", { status: 400 });
+  }
+
+  const prompt = `Generate 5 questions based on the content of the link: ${url}`;
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    max_tokens: 200,
+    stream: false,
+  };
+
+  const res = await fetch("https://api.openai.com/v1/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const data = res.body;
+
+  return new Response(data, {
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+  });
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-green-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
