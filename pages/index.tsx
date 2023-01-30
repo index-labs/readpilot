b040e6f9101df8186066d5d339de47299f4293b6
Jiayuan <forrestchang7@gmail.com>
2023-01-31 02:51:35 +0800

    feat: update analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -122,11 +122,11 @@ export default function Home() {
           >
             <Balancer>
               <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text font-bold text-transparent">
-                3720
+                5980
               </span>{" "}
               Users Analyzed{" "}
               <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text font-bold text-transparent">
-                5223
+                8204
               </span>{" "}
               Articles
             </Balancer>
