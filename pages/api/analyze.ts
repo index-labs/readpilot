431984605cfd0e29129dc12dcf564cf6085538b4
Jiayuan <forrestchang7@gmail.com>
2023-02-02 03:58:52 +0800

    feat: migrate analyze_url to pipe3 backend (#12)
    * feat: migrate analyze_url to pipe3 backend
    
    * fix: remove openai key
    
    * fix: some issues
    
    * feat: update analytics

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -13,48 +13,31 @@ const handler = async (req: NextRequest) => {
     });
   }
 
-  const prompt = `
-  Generate a list of thought provoking discussion questions about the URL, and return the answers of these questions with the evidence.
+  const backendDomain = process.env.BACKEND_DOMAIN as string;
+  const apiUrl = `${backendDomain}/api/v1/readpilot/analyze_url`;
 
-  Please generate a JSON list object with the following properties: q and a. q and a should be string. q is the question. a is the answer.
-
-  The URL is: ${url}
-  `;
-
-  const payload = {
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 500,
-    stream: false,
-    n: 1,
-  };
-
-  const res = await fetch("https://api.openai.com/v1/completions", {
+  const res = await fetch(apiUrl, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      "X-Api-Token": `${process.env.PIPE3_API_KEY}`,
     },
-    method: "POST",
-    body: JSON.stringify(payload),
+    body: JSON.stringify({
+      url: url,
+    }),
   });
 
-  if (!res.ok) {
-    return new Response(JSON.stringify({ msg: "OpenAI API error", data: [] }), {
+  if (res.status != 200) {
+    console.log(`[analyze.ts] Pipe3 API error: ${res}`);
+    return new Response(JSON.stringify({ msg: "Pipe3 API error", data: [] }), {
       status: 500,
     });
   }
 
-  const { choices } = await res.json();
-  const { text } = choices[0];
-  const rawData = text.trimStart().replace(/^Output: /, "");
-  console.log(rawData);
-  const results = JSON.parse(rawData);
+  const respJson = await res.json();
+  const data = respJson.data;
 
-  return new Response(JSON.stringify({ data: results }));
+  return new Response(JSON.stringify({ data: data }));
 };
 
 export default handler;
